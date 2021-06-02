4d40b967f5d9faef1f76634470c659ac14153c3b
EdwinMindcraft <edwin.mindcraft@gmail.com>
2021-06-02 10:31:20 +0200

    Fix #25

--- forge/src/main/java/io/github/apace100/origins/mixin/forge/LivingEntityMixin.java
+++ forge/src/main/java/io/github/apace100/origins/mixin/forge/LivingEntityMixin.java
@@ -1,7 +1,5 @@
 package io.github.apace100.origins.mixin.forge;
 
-import io.github.apace100.origins.component.OriginComponent;
-import io.github.apace100.origins.power.ModifyJumpPower;
 import io.github.apace100.origins.power.PowerTypes;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -10,14 +8,12 @@ import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
@@ -34,9 +30,12 @@ public abstract class LivingEntityMixin extends Entity {
 			locals = LocalCapture.CAPTURE_FAILHARD,
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/attribute/EntityAttributeInstance;getValue()D", ordinal = 0))
 	public void doAvianSlowFall(Vec3d p_213352_1_, CallbackInfo ci, double d0, EntityAttributeInstance gravity, boolean flag) {
-		if(flag && PowerTypes.SLOW_FALLING.isActive(this)) {
+		if (flag && PowerTypes.SLOW_FALLING.isActive(this)) {
 			//Will be removed if neither the potion effect nor the power is active.
-			gravity.addTemporaryModifier(SLOW_FALLING);
+			//Currently these won't stack. It is technically possible to make it so they stack,
+			//but that won't keep parity between both versions.
+			if (!gravity.hasModifier(SLOW_FALLING))
+				gravity.addTemporaryModifier(SLOW_FALLING);
 			this.fallDistance = 0.0F;
 		}
 	}
