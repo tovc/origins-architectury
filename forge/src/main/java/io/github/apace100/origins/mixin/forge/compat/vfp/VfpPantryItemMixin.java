28550a2e90520e02720fb7a3a3d995f07eef76f9
EdwinMindcraft <edwin.mindcraft@gmail.com>
2021-07-17 13:25:15 +0200

    VFP Compat now works. Apparently refmaps can be created for other mods.

--- forge/src/main/java/io/github/apace100/origins/mixin/forge/compat/vfp/VfpPantryItemMixin.java
+++ forge/src/main/java/io/github/apace100/origins/mixin/forge/compat/vfp/VfpPantryItemMixin.java
@@ -3,7 +3,6 @@ package io.github.apace100.origins.mixin.forge.compat.vfp;
 import io.github.apace100.origins.power.ModifyFoodPower;
 import io.github.apace100.origins.registry.ModComponentsArchitectury;
 import io.github.apace100.origins.util.AttributeUtil;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -16,17 +15,17 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Mixin(VfpPantryItem.class)
+@Mixin(value = VfpPantryItem.class)
 public class VfpPantryItemMixin {
 
-	@ModifyArgs(method = "onPortionConsumed", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;add(IF)V"), remap = false)
-	public void alterFood(Args args, ItemStack eaten, int foodAmount, float foodSaturation, World world, PlayerEntity player) {
+	@ModifyArgs(method = "onPortionConsumed", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;add(IF)V"))
+	private void origin$alterFood(Args args, ItemStack eaten, int foodAmount, float foodSaturation, World world, PlayerEntity player) {
 		int foodArg = args.size() - 2;
 		int satArg = args.size() - 1;
-		if(player != null) {
+		if (player != null) {
 			List<ModifyFoodPower> powers = ModComponentsArchitectury.getOriginComponent(player).getPowers(ModifyFoodPower.class).stream().filter(p -> p.doesApply(eaten)).collect(Collectors.toList());
-			args.set(foodArg, AttributeUtil.sortAndApplyModifiers(powers.stream().flatMap(x -> x.getFoodModifiers().stream()).collect(Collectors.toList()), args.<Integer>get(foodArg)));
-			args.set(satArg, AttributeUtil.sortAndApplyModifiers(powers.stream().flatMap(x -> x.getSaturationModifiers().stream()).collect(Collectors.toList()), args.<Float>get(satArg)));
+			args.set(foodArg, (int) AttributeUtil.sortAndApplyModifiers(powers.stream().flatMap(x -> x.getFoodModifiers().stream()).collect(Collectors.toList()), args.<Integer>get(foodArg)));
+			args.set(satArg, (float) AttributeUtil.sortAndApplyModifiers(powers.stream().flatMap(x -> x.getSaturationModifiers().stream()).collect(Collectors.toList()), args.<Float>get(satArg)));
 		}
 	}
 }
